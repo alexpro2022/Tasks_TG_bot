745c17e2ad3b32afc3b003d48cc5de7bf8ec827c
Alex Proskuriakov <alexpro1972@yandex.ru>
2024-05-03 11:05:48 +0300

    Improve app structure

--- app/bot/handlers/tasks.py
+++ app/bot/handlers/tasks.py
@@ -1,13 +1,31 @@
 from aiogram import Router, filters, types
+from constants import ADD_TASK_MSG, TASK_LIST_MSG
+from services import tasks
+
+from .. import utils as u
 
 router = Router(name=__name__)
 
 
 @router.message(filters.Command("add"))
-async def add(message: types.Message) -> None:
-    await message.answer("add")
+async def add_task_handler(message: types.Message) -> None:
+    """`/add` command handler. Starts dialog and saves the input to DB."""
+    # TODO: start fms, get task name and description, close fms
+    task_name = "TASK"
+    task_descr = "TASK DESCRIPTION"
+    await tasks.save_task(task_name, task_descr)
+    text = ADD_TASK_MSG.format(
+        username=u.get_username(message),
+        task=f"{task_name}\n{task_descr}\n",
+    )
+    await message.answer(text)
 
 
 @router.message(filters.Command("tsk"))
-async def tasks(message: types.Message) -> None:
-    await message.answer("tsk")
+async def list_tasks_handler(message: types.Message) -> None:
+    """`/tsk` command handler. Sends the message with the list of all tasks names."""
+    text = TASK_LIST_MSG.format(
+        username=u.get_username(message),
+        tasks=await tasks.get_all(),
+    )
+    await message.answer(text)
---
+++ app/services/tasks.py
@@ -0,0 +1,7 @@
+async def get_all() -> list[str]:
+    return ["TASK1", "TASK2", "TASK3"]
+
+
+async def save_task(task_name: str, task_description: str | None = None) -> None:
+    # TODO: save task to DB
+    pass
