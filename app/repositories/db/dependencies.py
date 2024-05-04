bd96c7afdcbf792f61bc8f445523d04987baf330
Alex Proskuriakov <alexpro1972@yandex.ru>
2024-05-04 13:47:10 +0300

    Add crud

--- app/repositories/db/dependencies.py
+++ app/repositories/db/dependencies.py
@@ -1,13 +1,6 @@
-from typing import AsyncGenerator
-
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from .config import db_conf
 
-engine = create_async_engine(db_conf.database_url)
-AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncSessionLocal() as async_session:
-        yield async_session
+engine = create_async_engine(db_conf.database_url, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
