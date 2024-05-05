24cbe2af0b7222279b0d5f2d495de9810f9df015
Alex Proskuriakov <alexpro1972@yandex.ru>
2024-05-05 21:03:44 +0300

    Rearrange fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,14 @@
-pytest_plugins = [
-    "tests.fixtures",
-    # "fixtures",
-]
+import pytest
+from aiogram import Bot, Dispatcher
+
+from app.bot.dispatcher import get_dispatcher
+
+
+@pytest.fixture(scope="session")
+def bot() -> Bot:
+    return Bot(token="1111111111:AAAAAAAAAAAAAAAAA-BBB_CCCCCCCCCCCCC")
+
+
+@pytest.fixture(scope="session")
+def dispatcher() -> Dispatcher:
+    return get_dispatcher()
