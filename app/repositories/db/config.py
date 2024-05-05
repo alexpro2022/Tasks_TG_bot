8bcd9e1e3a4ccfbe90761cda32150f21c7408131
Alex Proskuriakov <alexpro1972@yandex.ru>
2024-05-05 21:13:15 +0300

    Add default values for DB settings (Pydantic validation requirements)

--- app/repositories/db/config.py
+++ app/repositories/db/config.py
@@ -3,11 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    postgres_user: SecretStr  # = 'postgres'
-    postgres_password: SecretStr  # = 'postgrespw'
+    postgres_user: SecretStr = "postgres"
+    postgres_password: SecretStr = "postgrespw"
     db_host: str = "db"  # database service name in docker-compose.yml
-    db_port: SecretStr  # = '5432'
-    db_name: SecretStr  # = 'postgres'
+    db_port: SecretStr = "5432"
+    db_name: SecretStr = "postgres"
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
